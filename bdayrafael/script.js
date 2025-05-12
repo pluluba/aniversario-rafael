8fb841753ec1227f975e829a5dc0c73b9798f5be
pluluba <122917457+pluluba@users.noreply.github.com>
2025-05-11 21:02:50 -0300

    Add files via upload

---
+++ bdayrafael/script.js
@@ -0,0 +1,7 @@
+function abrirModal() {
+    document.getElementById("modal").style.display = "flex";
+  }
+  
+  function fecharModal() {
+    document.getElementById("modal").style.display = "none";
+  }
